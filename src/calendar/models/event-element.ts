e4acab3832626796b4d95740eef63e3d7dfee83f
Kapil Neurgaonkar <kapil.neurgaonkar@gmail.com>
2021-12-27 01:59:13 +0530

    Displayed event on calendar week view

---
+++ src/calendar/models/event-element.ts
@@ -0,0 +1,13 @@
+export interface EventElement {
+  title: string;
+  style: {
+    width: string;
+    height: number;
+    marginTop: string;
+  };
+  textStyle: {
+    fontSize: number;
+    fontWeight: number;
+    lineHeight: number;
+  };
+}
