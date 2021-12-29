5bb04ff1f2298178e50bb613c68a8f419b17c3da
Kapil Neurgaonkar <kapil.neurgaonkar@gmail.com>
2021-12-29 17:32:11 +0530

    Performance improvement in grouping events

--- src/calendar/components/week-view-content.tsx
+++ src/calendar/components/week-view-content.tsx
@@ -1,15 +1,68 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import { Grid, Paper } from '@mui/material';
 
-import WeekViewConstants from '../constants/week-view-constants';
 import WeekContext from '../contexts/week-context';
+import EventsContext from '../contexts/events-context';
 import WeekViewEvents from './week-view-events';
 import WeekViewCurrentTimeline from './week-view-current-timeline';
 import CalendarUtils from '../services/calendar-utils';
+import { EventElement } from '../models/event-element';
+import { IndexedCalendarEvent } from '../../events/db/calendar-db';
+import WeekViewConstants from '../constants/week-view-constants';
 
 const WeekViewContent: React.FC = () => {
   const { week } = useContext(WeekContext);
+  const { events } = useContext(EventsContext);
+  const [dateWiseEventElements, setDateWiseEventElements] =
+    useState<Map<number, EventElement[]>>();
+
+  useEffect(() => {
+    const eventElementMap = new Map<number, EventElement[]>();
+    events.forEach((event) => {
+      const eventDate = new Date(event.startTime).getDate();
+      let existingEventElements = eventElementMap.get(eventDate);
+      if (!existingEventElements) {
+        existingEventElements = [];
+        eventElementMap.set(eventDate, existingEventElements);
+      }
+      existingEventElements.push(getEventElement(event));
+    });
+    setDateWiseEventElements(eventElementMap);
+  }, [events]);
+
+  const getEventElement = (event: IndexedCalendarEvent): EventElement => {
+    const durationInMins =
+      (new Date(event.endTime).getTime() -
+        new Date(event.startTime).getTime()) /
+      1000 /
+      60;
+    const timeSinceMidnight = CalendarUtils.getMinutesSinceMidnight(
+      new Date(event.startTime)
+    );
+
+    const textStyle =
+      durationInMins > 15
+        ? {
+            fontSize: 12,
+            fontWeight: 500,
+            lineHeight: 1.5,
+          }
+        : {
+            fontSize: 10,
+            fontWeight: 400,
+            lineHeight: 1.1,
+          };
+    return {
+      title: event.title,
+      style: {
+        width: '135px',
+        height: Math.max((40 / 60) * durationInMins, 10),
+        marginTop: (40 / 60) * timeSinceMidnight + 'px',
+      },
+      textStyle,
+    } as EventElement;
+  };
 
   return (
     <>
@@ -27,7 +80,9 @@ const WeekViewContent: React.FC = () => {
             ></WeekViewCurrentTimeline>
 
             {/* List events */}
-            <WeekViewEvents weekday={weekday}></WeekViewEvents>
+            <WeekViewEvents
+              eventElements={dateWiseEventElements?.get(weekday.getDate())}
+            ></WeekViewEvents>
 
             {/* Show hour slots for a week day */}
             {CalendarUtils.get24HourSlots().map((hour) => {
--- src/calendar/components/week-view-events.tsx
+++ src/calendar/components/week-view-events.tsx
@@ -1,94 +1,39 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 
 import { Typography, Paper } from '@mui/material';
 
-import CalendarUtils from '../services/calendar-utils';
 import { EventElement } from '../models/event-element';
-import { CalendarEvent } from '../models/calendar-event';
-import EventsContext from '../contexts/events-context';
 
 const WeekViewEvents: React.FC<{
-  weekday: Date;
-}> = ({ weekday }) => {
-  const { events } = useContext(EventsContext);
-  const [dateWiseEventElements, setDateWiseEventElements] =
-    useState<Map<string, EventElement[]>>();
-
-  useEffect(() => {
-    const eventElementMap = new Map<string, EventElement[]>();
-    events.forEach((event) => {
-      const eventDate = new Date(event.startTime).toDateString();
-      let existingEventElements = eventElementMap.get(eventDate);
-      if (!existingEventElements) {
-        existingEventElements = [];
-        eventElementMap.set(eventDate, existingEventElements);
-      }
-      existingEventElements.push(getEventElement(event));
-    });
-    setDateWiseEventElements(eventElementMap);
-  }, [events]);
-
-  const getEventElement = (event: CalendarEvent): EventElement => {
-    const durationInMins =
-      (new Date(event.endTime).getTime() -
-        new Date(event.startTime).getTime()) /
-      1000 /
-      60;
-    const timeSinceMidnight = CalendarUtils.getMinutesSinceMidnight(
-      new Date(event.startTime)
-    );
-
-    const textStyle =
-      durationInMins > 15
-        ? {
-            fontSize: 12,
-            fontWeight: 500,
-            lineHeight: 1.5,
-          }
-        : {
-            fontSize: 10,
-            fontWeight: 400,
-            lineHeight: 1.1,
-          };
-    return {
-      title: event.title,
-      style: {
-        width: '135px',
-        height: Math.max((40 / 60) * durationInMins, 10),
-        marginTop: (40 / 60) * timeSinceMidnight + 'px',
-      },
-      textStyle,
-    } as EventElement;
-  };
+  eventElements?: EventElement[];
+}> = ({ eventElements }) => {
   return (
     <>
-      {dateWiseEventElements
-        ?.get(weekday.toDateString())
-        ?.map((eventElement, index) => {
-          return (
-            <Paper
-              key={index}
-              elevation={1}
+      {eventElements?.map((eventElement, index) => {
+        return (
+          <Paper
+            key={index}
+            elevation={1}
+            sx={{
+              color: '#fff',
+              bgcolor: '#039be5',
+              position: 'absolute',
+              marginBottom: '2px',
+              ...eventElement.style,
+            }}
+          >
+            <Typography
+              component={'h6'}
               sx={{
-                color: '#fff',
-                bgcolor: '#039be5',
-                position: 'absolute',
-                marginBottom: '2px',
-                ...eventElement.style,
+                padding: '0 4px',
+                ...eventElement.textStyle,
               }}
             >
-              <Typography
-                component={'h6'}
-                sx={{
-                  padding: '0 4px',
-                  ...eventElement.textStyle,
-                }}
-              >
-                {eventElement.title}
-              </Typography>
-            </Paper>
-          );
-        })}
+              {eventElement.title}
+            </Typography>
+          </Paper>
+        );
+      })}
     </>
   );
 };
