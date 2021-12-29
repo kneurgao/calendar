91c5fb8d9e5141f25ac0e5d2fb1f0230cf4742ab
Kapil Neurgaonkar <kapil.neurgaonkar@gmail.com>
2021-12-29 15:43:02 +0530

    Week view content component code cleanup

---
+++ src/calendar/components/week-view-current-timeline.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+
+import { Paper } from '@mui/material';
+
+import CalendarUtils from '../services/calendar-utils';
+
+const WeekViewCurrentTimeline: React.FC<{
+  weekday: Date;
+}> = ({ weekday }) => {
+  const [currentTimeline, setCurrentTimeline] = useState<number>(0);
+
+  useEffect(() => {
+    const updateCurrentTimeline = () => {
+      const minutesSinceMidnight = CalendarUtils.getMinutesSinceMidnight();
+      setCurrentTimeline((40 / 60) * minutesSinceMidnight - 1);
+    };
+    updateCurrentTimeline();
+    const currentTimelineTimer = setInterval(updateCurrentTimeline, 1000 * 60);
+    return () => {
+      clearInterval(currentTimelineTimer);
+    };
+  }, []);
+
+  return (
+    <>
+      {CalendarUtils.isToday(weekday) && (
+        <Paper
+          elevation={0}
+          sx={{
+            bgcolor: '#ea4335',
+            position: 'absolute',
+            zIndex: 2,
+            width: '11%',
+            height: '2px',
+            marginLeft: '2px',
+            marginTop: currentTimeline + 'px',
+          }}
+        ></Paper>
+      )}
+    </>
+  );
+};
+
+export default WeekViewCurrentTimeline;
